0827171e186dc5ceb3f1a2b42ba540dbd25072f0
Deyvison Iury <iurydeyvison@gmail.com>
2023-07-12 17:15:59 -0300

    Added shadcn librarie and changed previous styling of components

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,24 @@
+'use client'
+
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 export function Search() {
+
+  async function submitSearch(e: any) {
+    e.preventDefault();
+
+    console.log("Submiting...")
+  }
+
   return (
-    <div className="max-w-sm h-10 flex gap-1">
-      <input type="search" className="w-[303px] h-10 rounded border border-neutral-700 bg-neutral-900" />
-      <button 
-        className="flex justify-center w-10 h-10 left-[312px] bg-purple-400 rounded border-2 border-neutral-700       ">
-        <img src="iconSearch.svg"  className="w-4"/>
-      </button>
+    <div >
+      <form className="max-w-sm h-10 flex gap-1" onSubmit={(e) => submitSearch(e)}>
+        <Input type="text" placeholder="Search..."  />
+        <Button type="submit" >
+          <img src="iconSearch.svg"  className="w-4"/>
+        </Button>
+      </form>
     </div>
   )
 }
--- src/components/TopClipsSelect.tsx
+++ src/components/TopClipsSelect.tsx
@@ -1,42 +1,28 @@
 'use client'
 
-import * as Select from '@radix-ui/react-select'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
 
 export function TopClipsSelect() {
    return (
-    <Select.Root>
-      <Select.Trigger >
-        <Select.Value placeholder="Top" />
-        <Select.Icon >
-
-        </Select.Icon>
-      </Select.Trigger>
-      <Select.Portal>
-        <Select.Content className='overflow-hidden rounded-md top-10 bg-neutral-800 p-0 m-0' >
-          <Select.Viewport className='p-2 flex flex-col align-items-center w-100'>
-              <Select.Item value="day" className='data-[highlighted]:bg-neutral-500'>
-                <Select.ItemText>
+    <Select defaultValue="day" >
+      <SelectTrigger className="w-30 gap-1">
+        Top
+        <SelectValue />
+      </SelectTrigger>
+        <SelectContent >
+              <SelectItem value="day" >
                   24 hours
-                </Select.ItemText>
-              </Select.Item>
-              <Select.Item value="week" className='data-[highlighted]:bg-neutral-500'>
-                <Select.ItemText>
+              </SelectItem>
+              <SelectItem value="week" >
                   7 days
-                </Select.ItemText>
-              </Select.Item>
-              <Select.Item value="month" className='data-[highlighted]:bg-neutral-500'>
-                <Select.ItemText>
+              </SelectItem>
+              <SelectItem value="month" >
                   Month 
-                </Select.ItemText>
-              </Select.Item>
-              <Select.Item value="all" className='data-[highlighted]:bg-neutral-500'>
-                <Select.ItemText>
+              </SelectItem>
+              <SelectItem value="all" >
                   All 
-                </Select.ItemText>
-              </Select.Item>
-          </Select.Viewport>
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
+              </SelectItem>
+        </SelectContent>
+    </Select>
    )
 }
